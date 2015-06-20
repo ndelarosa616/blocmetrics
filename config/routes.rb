f6a5aec59c89df2e15d227e8f2b3408c710f9940
Nicole De La Rosa <nicoledelarosa616@gmail.com>
2015-06-20 12:40:34 -0400

    added api and events controller

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,9 @@ Rails.application.routes.draw do
     root to: 'welcome#index'
   end
 
+  namespace :api, defaults: { format: :json } do
+    match 'create_event', to: 'events#create', via: [:options]
+    resources :events, only: [:create]
+  end
+
 end
